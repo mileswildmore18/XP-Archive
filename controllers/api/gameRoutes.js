f70d8fbcfcb7e29354d9da2fb3d7671ba42fdc97
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-05-15 13:39:59 -0400

    finished the add game route

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -5,16 +5,20 @@ const { Games } = require('../../models');
 
 router.post('/', withAuth, async (req, res) => {
     try {
-        console.log('creating game', req.body)
-      const newGame = await Games.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newGame);
+
+        const game = req.body;
+        game.user_id = req.session.user_id
+        console.log('creating game', game)
+        const newGame = await Games.create({
+            ...req.body, 
+            user_id: req.session.user_id
+        });
+
+        res.status(200).json(game);
     } catch (err) {
-      res.status(400).json(err);
+        console.log(err)
+        res.status(400).json(err);
     }
-  });
+});
 
 module.exports = router;
